5f94e53c4df37744fb5982736d9e8fe97fefd4ce
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2024-08-30 20:59:53 +0200

    added additional tests for CLI and API and test data (barley and human genes)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,10 @@
 import blast2galaxy
-import json
 
 test_tool_id = 'toolshed.g2.bx.psu.edu/repos/bgruening/diamond/bg_diamond/2.0.15+galaxy0'
 test_db_id = 'uniprot_swissprot_2023_03'
 
 
-dna_seq = """>Vrs1_EU886278.1
+dna_seq_barley = """>Vrs1_EU886278.1
 ATGGACAAGCATCAGCTCTTTGATTCATCCAACGTGGACACGACTTTCTTCGCGGCCAATGGCACGGCGC
 AGGGGGATACCAGCAAGCAGAGGGCGCGGCGCAGGCGGCGGAGGTCGGCGAGGTGCGGCGGAGGGGATGG
 TGACGGTGGGGAGATGGACGGAGGAGGGGACCCCAAGAAGCGGCGGCTCACCGACGAGCAGGCCGAGATT
@@ -17,15 +16,20 @@ GCCACGGGGCATCTGTGGATGGCGGACACGCCGCTGGCGCCGTTGGCGTGTGCGGCGGGAGCCCGAGCTC
 GTCCTTCTCGACGGGAACCTGCCAGCAGCAGCCGGGTTTCAGCGGGGCAGACGTGCTGGGGCGGGACGAT
 GACCTGATGATGTGCGTCCCCGAGTGGTTTTTAGCATGA"""
 
+protein_seq_barley = """>Vrs1_EU886278.1
+MDKHQLFGSSNVDTTFFAANGTAQGETSKQRARRRRRRSARCGGGDGDGGEMDGGGDPKKRRLTDEQAEI
+LELSFREDRKLETARKVYLAAELGLDPKQVAVWFQNRRARHKNKTLEEEFARLKHAHDAAILHKCHLENE
+LLRLKERLGATEQEVRRLRSAAGSHGASVDGGHAAGAVGVCGGSPSSSFSTGTCQQQPGFSGADVLGRDD
+DLMMCVPEWFLA"""
 
-protein_seq = """>sp|P62805|H4_HUMAN Histone H4 OS=Homo sapiens OX=9606 GN=H4C1 PE=1 SV=2
+
+protein_seq_human = """>sp|P62805|H4_HUMAN Histone H4 OS=Homo sapiens OX=9606 GN=H4C1 PE=1 SV=2
 MSGRGKGGKGLGKGGAKRHRKVLRDNIQGITKPAIRRLARRGGVKRISGLIYEETRGVLK
 VFLENVIRDAVTYTEHAKRKTVTAMDVVYALKRQGRTLYGFGG"""
 
 
 def test_list_tools_api():
     tools = blast2galaxy.list_tools()
-    #print(json.dumps(tools, indent=4))
 
     tool = False
     db = False
@@ -39,11 +43,10 @@ def test_list_tools_api():
     assert (tool, db) == (True, True)
 
 
-
 def test_blastn():
     result = blast2galaxy.blastn(
         profile = 'blastn_barley',
-        query_str = dna_seq,
+        query_str = dna_seq_barley,
         db = 'morex_v3.all.cds',
         outfmt = '6'
     )
@@ -51,19 +54,46 @@ def test_blastn():
     assert 'HORVU.MOREX.r3.2HG0184740.1' in result
 
 
+def test_tblastn():
+    result = blast2galaxy.tblastn(
+        profile = 'tblastn_barley',
+        query_str = protein_seq_barley,
+        db = 'morex_v3.all.cds',
+        outfmt = '6'
+    )
+
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result
+    assert 'HORVU.MOREX.r3.2HG0131280.1' in result
+
+
+def test_blastx():
+    result = blast2galaxy.blastx(
+        profile = 'blastx_barley',
+        query_str = dna_seq_barley,
+        db = 'morex_v3.all',
+        outfmt = '6'
+    )
+
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result
+    assert 'HORVU.MOREX.r3.2HG0131280.1' in result
+
+
+def test_blastp():
+    result = blast2galaxy.blastp(
+        profile = 'blastp_barley',
+        query_str = protein_seq_barley,
+        db = 'morex_v3.all',
+        outfmt = '6'
+    )
+
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result
+    assert 'HORVU.MOREX.r3.2HG0131280.1' in result
+
 
 def test_diamond_blastp():
-
-    # res = blast2galaxy.diamond_blastp(
-    #     profile = 'diamond_blastp',
-    #     query = 'query_protein.fasta',
-    #     db = 'uniprot_swissprot_2023_03', # Hordeum_vulgare__BPGv2__all_BPGv2:pep
-    #     outfmt = '6'
-    # )
-
     result = blast2galaxy.diamond_blastp(
         profile = 'diamond_blastp',
-        query_str = protein_seq,
+        query_str = protein_seq_human,
         db = 'uniprot_swissprot_2023_03',
         outfmt = '6'
     )
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,82 @@
 from blast2galaxy import cli
 from click.testing import CliRunner
-import shutil
 
 test_tool_id = 'toolshed.g2.bx.psu.edu/repos/bgruening/diamond/bg_diamond/2.0.15+galaxy0'
+
 
 def test_list_tools_cli():
     runner = CliRunner()
     result = runner.invoke(cli.list_tools, terminal_width=1000)
-    #print(result.exit_code)
-    #print(result.output)
+
     assert result.exit_code == 0
     assert test_tool_id in result.output
+
+
+def test_blastn_cli():
+    runner = CliRunner()
+    params = [
+        '--profile', 'blastn_barley',
+        '--query', 'test_data/barley_vrs1_cds.fasta',
+        '--db', 'morex_v3.all.cds',
+        '--outfmt', '6'
+    ]
+    result = runner.invoke(cli.blastn, params, terminal_width=1000)
+
+    assert result.exit_code == 0
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result.output
+
+
+def test_tblastn_cli():
+    runner = CliRunner()
+    params = [
+        '--profile', 'tblastn_barley',
+        '--query', 'test_data/barley_vrs1_prot.fasta',
+        '--db', 'morex_v3.all.cds',
+        '--outfmt', '6'
+    ]
+    result = runner.invoke(cli.tblastn, params, terminal_width=1000)
+
+    assert result.exit_code == 0
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result.output
+
+
+def test_blastx_cli():
+    runner = CliRunner()
+    params = [
+        '--profile', 'blastx_barley',
+        '--query', 'test_data/barley_vrs1_cds.fasta',
+        '--db', 'morex_v3.all',
+        '--outfmt', '6'
+    ]
+    result = runner.invoke(cli.blastx, params, terminal_width=1000)
+
+    assert result.exit_code == 0
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result.output
+
+
+def test_blastp_cli():
+    runner = CliRunner()
+    params = [
+        '--profile', 'blastp_barley',
+        '--query', 'test_data/barley_vrs1_prot.fasta',
+        '--db', 'morex_v3.all',
+        '--outfmt', '6'
+    ]
+    result = runner.invoke(cli.blastp, params, terminal_width=1000)
+
+    assert result.exit_code == 0
+    assert 'HORVU.MOREX.r3.2HG0184740.1' in result.output
+
+
+def test_diamond_blastp_cli():
+    runner = CliRunner()
+    params = [
+        '--profile', 'diamond_blastp',
+        '--query', 'test_data/human_h4c1_prot.fasta',
+        '--db', 'uniprot_swissprot_2023_03',
+        '--outfmt', '6'
+    ]
+    result = runner.invoke(cli.diamond_blastp, params, terminal_width=1000)
+
+    assert result.exit_code == 0
+    assert 'sp|P62803|H4_BOVIN' in result.output
